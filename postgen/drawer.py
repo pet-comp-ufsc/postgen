ce3299402f6175b0532759259313377c47a46bd9
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-05-02 12:27:53 -0300

    Add possibility to set description line height.

--- postgen/drawer.py
+++ postgen/drawer.py
@@ -4,7 +4,7 @@ from enum import Enum
 from io import BytesIO
 from pathlib import Path
 from textwrap import wrap
-from typing import Tuple, NamedTuple, Union
+from typing import Tuple, NamedTuple, Optional, Union
 
 from cairosvg import svg2png
 from PIL import Image, ImageDraw, ImageFont, ImagePath
@@ -56,9 +56,19 @@ class Size(NamedTuple):
 Color = Union[Tuple[int, int, int, int], int, str]
 
 
+def or_default(cls, data, field, *keys):
+    try:
+        for key in keys:
+            data = data[key]
+        return data
+    except KeyError:
+        return Theme._field_defaults[field]
+
+
 class Theme(NamedTuple):
     fgcolor: Color = (255, 255, 255, 255)
     bgcolor: Color = (95, 166, 219, 255)
+    line_height: int = 16
 
     @staticmethod
     def from_path(path: Path) -> 'Theme':
@@ -74,6 +84,13 @@ class Theme(NamedTuple):
             return Theme(
                 fgcolor=colors['fgcolor'],
                 bgcolor=colors['bgcolor'],
+                line_height=or_default(
+                    Theme,
+                    theme,
+                    'line_height',
+                    'description',
+                    'line-height'
+                )
             )
 
         if path is None:
@@ -89,7 +106,7 @@ class Theme(NamedTuple):
         raise ThemeNotFound(path)
 
 
-def apply_relatives(pos: Pos, size: Size, obj_size: Size):
+def apply_relatives(pos: Union[Pos, Tuple[RelativePos, RelativePos]], size: Size, obj_size: Size):
     '''Checks whether an object's position has any RelativePos and applies it
     within canvas and the object size.'''
     x, y = pos
@@ -147,13 +164,13 @@ class Drawer:
     def draw_text(
             self,
             text: str,
-            pos: Pos,
-            font: Font = None,
+            pos: Union[Pos, RelativePos],
+            font_data: Optional[Font] = None,
             align: Align = Align.CENTER,
             ):
-        if font:
+        if font_data:
             try:
-                font = ImageFont.truetype(font.family, size=font.size)
+                font = ImageFont.truetype(font_data.family, size=font_data.size)
                 not_found = False
             except OSError:
                 not_found = True
@@ -172,7 +189,8 @@ class Drawer:
             text,
             fill=self.theme.fgcolor,
             font=font,
-            align=align.value
+            align=align.value,
+            spacing=self.theme.line_height,
         )
 
     def draw_path(self, path: ImagePath, fill=None, outline=None):
@@ -197,19 +215,19 @@ def make_image(post: Post, size: Size, theme: Theme = Theme()):
             (0, 120),
             (120, 0),
         ]),
-        fill=(125, 196, 249, 255),
+        fill=theme.fgcolor,
     )
 
     drawer.draw_text(
         post.title,
         Pos(RelativePos.Center(), 120),
-        font=Font(family='DejaVuSans', size=72),
+        font_data=Font(family='DejaVuSans', size=72),
     )
 
     drawer.draw_text(
         fit(post.description, 28),
         Pos(RelativePos.Center(), 360),
-        font=Font(family='DejaVuSans', size=48),
+        font_data=Font(family='DejaVuSans', size=48),
     )
 
     drawer.draw_image(
